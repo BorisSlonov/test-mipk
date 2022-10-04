0c02dcda4a3e205342a34e0617a8ed4b23209d3c
Boris <sslonovborisss@gmail.com>
2022-10-04 16:09:53 +0300

    add menu

---
+++ js/main.js
@@ -0,0 +1,27 @@
+﻿"use strict"
+
+//burger
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__list'),
+        menuItem = document.querySelectorAll('.header__link'),
+        hamburger = document.querySelector('.header__burger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('burger_active');
+        menu.classList.toggle('menu_active');
+        document.querySelector('.header__btn').classList.toggle('header__btn_active')
+        document.querySelector('.header-top__item_logo').classList.toggle('header__logo_active')
+        document.querySelector('.header-top__item_btn').classList.toggle('header-top__item_btn_active')
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('burger_active');
+            menu.classList.toggle('menu_active');
+
+            document.querySelector('.header__btn').classList.toggle('header__btn_active')
+            document.querySelector('.header-top__item_logo').classList.toggle('header__logo_active')
+            document.querySelector('.header-top__item_btn').classList.toggle('header-top__item_btn_active')
+        })
+    })
+})
